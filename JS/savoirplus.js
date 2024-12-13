84465b997115786f1d90040f571f1324484c5ec1
uno4n4 <sortlcrossvolee@gmail.com>
2024-12-13 22:25:10 +0100

    Ajout de la page js complète

---
+++ JS/savoirplus.js
@@ -0,0 +1,38 @@
+const toggleNavButton = document.getElementById("toggle-nav");
+const sideNav = document.querySelector(".side-nav");
+
+toggleNavButton.addEventListener("click", () => {
+    sideNav.classList.toggle("open");
+});
+
+const toggleDaltonienModeBtn = document.getElementById('toggleDaltonienMode');
+toggleDaltonienModeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('daltonien-mode');
+});
+
+
+const zoomInBtn = document.getElementById('zoomIn');
+const zoomOutBtn = document.getElementById('zoomOut');
+let zoomLevel = 1;
+
+zoomInBtn.addEventListener('click', () => {
+  if (zoomLevel < 2) { 
+    zoomLevel += 0.1;
+    document.body.style.transform = `scale(${zoomLevel})`;
+    document.body.style.transformOrigin = '0 0';
+  }
+});
+
+zoomOutBtn.addEventListener('click', () => {
+  if (zoomLevel > 1) { 
+    zoomLevel -= 0.1;
+    document.body.style.transform = `scale(${zoomLevel})`;
+    document.body.style.transformOrigin = '0 0';
+  }
+});
+
+
+const toggleDarkModeBtn = document.getElementById('toggleDarkMode');
+toggleDarkModeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
